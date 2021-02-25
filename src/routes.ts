486dc45f31bb639875474549d902e5925d6b730e
George W F Santos <gweverton21@hotmail.com>
2021-02-24 21:19:52 -0300

    refactor UserController and create repositories for both user and survey entities

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import UserController from "./controllers/UserController";
+import SurveysController from "./controllers/SurveysController";
+import UsersController from "./controllers/UsersController";
 
 const routes = Router();
 
-routes.post("/users", UserController.create);
+routes.post("/users", UsersController.create);
+
+routes.post("/surveys", SurveysController.create);
+routes.get("/surveys", SurveysController.show);
 
 export default routes;
