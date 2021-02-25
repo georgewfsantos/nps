fa07dfbf06460f11bdb04fc5d23418d5ae5d2659
George W F Santos <gweverton21@hotmail.com>
2021-02-25 00:25:25 -0300

    write tests

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,29 @@
+import request from "supertest";
+import app from "../app";
+
+import createConnection from "../database";
+
+describe("Users", () => {
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  it("Should be able to create a new user", async () => {
+    const response = await request(app).post("/users").send({
+      email: "johndoe.com",
+      name: "John Doe",
+    });
+
+    expect(response.status).toBe(201);
+  });
+
+  it("should not be able to create new user if provided email is already taken", async () => {
+    const response = await request(app).post("/users").send({
+      email: "johndoe.com",
+      name: "John Doe",
+    });
+
+    expect(response.status).toBe(400);
+  });
+});
