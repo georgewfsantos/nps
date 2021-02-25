b222ccf17ef4eff8af320d4ec6650692e183d172
George W F Santos <gweverton21@hotmail.com>
2021-02-25 18:27:58 -0300

    create realtion between users and surveys and service to send mail

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import SendMailController from "./controllers/SendMailController";
 import SurveysController from "./controllers/SurveysController";
 import UsersController from "./controllers/UsersController";
 
@@ -8,5 +9,7 @@ routes.post("/users", UsersController.create);
 
 routes.post("/surveys", SurveysController.create);
 routes.get("/surveys", SurveysController.show);
+
+routes.post("/sendMail", SendMailController.execute);
 
 export default routes;
